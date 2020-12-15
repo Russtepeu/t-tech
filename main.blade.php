2dbcd27d75c0ce7630c7a4aa163d93ef7484a6ac
Leruss <39184966+Russtepeu@users.noreply.github.com>
2020-12-15 05:44:20 +0100

    Add files via upload

---
+++ main.blade.php
@@ -0,0 +1,111 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>leruss.com</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/js/app.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body class="font-perso bg-gray-400">
+    <div>
+        <div class="container mx-auto flex items-center py-4">
+        <nav class="w-2/5 font-perso">
+           <a href="#" class="top-nav-item">Home</a>
+           <a href="#" class="top-nav-item">Blog</a>
+           <a href="#" class="top-nav-item">About</a>
+           <a href="#" class="top-nav-item">Contact</a>
+        </nav>
+        </div>
+        <div class="hero h-20 bg-cover w-80">
+
+        </div>
+        <div class="container mx-auto flex mt-3">
+            <div class="w-3/4   mt-4  font-perso">
+                <div class="blog-post">
+                    <div class="blog-post-img">
+                       <img src="img/wao.png" alt="bouillon"> 
+                    </div>
+                     <div class="blog-post-info">
+                        <div class="blog-post-date">
+                            <span>Monday</span>
+                            <span>30 October 1992</span>
+                        </div> 
+                        <h1 class="blog-post-title text-lg font-semibold">titre de la publication</h1> 
+                        <p class="blog-post-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eius? Temporibus nihil sunt expedita earum porro velit alias cum repellendus quod non! A nostrum volu
+                        </p>
+                        <a href="#" class="blog-post-cta">Read More</a>
+                     </div>
+                </div>
+            </div>
+            <div class="w-1/4 pl-2  mt-2 mb-4 font-perso">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, voluptas soluta. Nisi, ex magnam, error quod eaque quidem voluptates nihil excepturi, mollitia ea quae repellat. Perferendis nam veniam quo debitis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, est doloremque, voluptates reiciendis minima quo reprehenderit quibusdam illo libero a, perspiciatis at illum repudiandae? Reprehenderit voluptates consequuntur animi incidunt asperiores!
+              <h1>je suis leruss</h1>
+            </div>
+        </div>
+        <div class="container mx-auto flex">
+            <div class="w-3/4    font-perso">
+                <div class="blog-post">
+                    <div class="blog-post-img">
+                       <img src="img/wao.png" alt="bouillon"> 
+                    </div>
+                     <div class="blog-post-info">
+                        <div class="blog-post-date">
+                            <span>Monday</span>
+                            <span>30 October 1992</span>
+                        </div> 
+                        <h1 class="blog-post-title text-lg font-semibold">titre de la publication</h1> 
+                        <p class="blog-post-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eius? Temporibus nihil sunt expedita earum porro velit alias cum repellendus quod non! A nostrum voluptatibus iure magni, cupiditate odit voluptates.
+                        </p>
+                        <a href="#" class="blog-post-cta">Read More</a>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mx-auto flex">
+            <div class="w-3/4    font-perso">
+                <div class="blog-post">
+                    <div class="blog-post-img">
+                       <img src="img/wao.png" alt="bouillon"> 
+                    </div>
+                     <div class="blog-post-info">
+                        <div class="blog-post-date">
+                            <span>Monday</span>
+                            <span>30 October 1992</span>
+                        </div> 
+                        <h1 class="blog-post-title text-lg font-semibold">titre de la publication</h1> 
+                        <p class="blog-post-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eius? Temporibus nihil sunt expedita earum porro velit alias cum repellendus quod non! A nostrum voluptatibus iure magni, cupiditate odit voluptates.
+                        </p>
+                        <a href="#" class="blog-post-cta">Read More</a>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mx-auto flex">
+            <div class="w-3/4    font-perso">
+                <div class="blog-post">
+                    <div class="blog-post-img">
+                       <img src="img/wao.png" alt="bouillon"> 
+                    </div>
+                     <div class="blog-post-info">
+                        <div class="blog-post-date">
+                            <span>Monday</span>
+                            <span>30 October 1992</span>
+                        </div> 
+                        <h1 class="blog-post-title text-lg font-semibold">titre de la publication</h1> 
+                        <p class="blog-post-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eius? Temporibus nihil sunt expedita earum porro velit alias cum repellendus quod non! A nostrum voluptatibus iure magni, cupiditate odit voluptates.
+                        </p>
+                        <a href="#" class="blog-post-cta">Read More </a>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @yield('content')
+</body>
+</html>
